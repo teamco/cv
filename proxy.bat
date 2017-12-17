eeeddff14e4659fd6fbdab647139c61f168eb9b4
Muhammad Wattad <MWATTAD@amdocs.com>
2017-12-17 12:44:04 +0200

    fix corporate proxy

--- proxy.bat
+++ proxy.bat
@@ -1,6 +1,7 @@
 @echo off
 set PROXY=http://genproxy:8080
-npm config set https-proxy http://genproxy:8080
-npm config set proxy http://genproxy:8080
-set HTTP_PROXY=http://genproxy:8080
-set HTTPS_PROXY=http://genproxy:8080
+git config --global http.proxy %PROXY%
+npm config set https-proxy %PROXY%
+npm config set proxy %PROXY%
+set HTTP_PROXY=%PROXY%
+set HTTPS_PROXY=%PROXY%

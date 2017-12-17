6b7644df8cc9a11d2675b1003ef80d88cc645158
Muhammad Wattad <MWATTAD@amdocs.com>
2017-12-17 12:41:10 +0200

    coninue init

--- proxy.bat
+++ proxy.bat
@@ -1,6 +1,6 @@
 @echo off
 set PROXY=http://genproxy:8080
-npm config set https-proxy %PROXY%
-npm config set proxy %PROXY%
-set HTTP_PROXY=%PROXY%
-set HTTPS_PROXY=%PROXY%
+npm config set https-proxy http://genproxy:8080
+npm config set proxy http://genproxy:8080
+set HTTP_PROXY=http://genproxy:8080
+set HTTPS_PROXY=http://genproxy:8080
